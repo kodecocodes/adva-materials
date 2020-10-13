08a15651c5733593d603b829515b2f25fd4090e8
Q <q@Q.qq>
2020-10-13 13:33:08 -0400

    add project files

---
+++ 13-user-privacy/projects/final/app/src/main/java/com/raywenderlich/android/petsave/report/presentation/ReportDetailFragment.kt
@@ -0,0 +1,228 @@
+/*
+ * Copyright (c) 2020 Razeware LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+ * distribute, sublicense, create a derivative work, and/or sell copies of the
+ * Software in any work that is designed, intended, or marketed for pedagogical or
+ * instructional purposes related to programming, coding, application development,
+ * or information technology.  Permission for such use, copying, modification,
+ * merger, publication, distribution, sublicensing, creation of derivative works,
+ * or sale is expressly withheld.
+ *
+ * This project and source code may use libraries or frameworks that are
+ * released under various Open-Source licenses. Use of those libraries and
+ * frameworks are governed by their own individual licenses.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package com.raywenderlich.android.petsave.report.presentation
+
+import android.Manifest
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Bundle
+import android.provider.MediaStore
+import android.text.InputType
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import com.raywenderlich.android.petsave.databinding.FragmentReportDetailBinding
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_report_detail.*
+import java.io.File
+import java.net.URL
+import java.util.*
+import java.util.concurrent.atomic.AtomicInteger
+import javax.net.ssl.HttpsURLConnection
+
+@AndroidEntryPoint
+class ReportDetailFragment : Fragment() {
+
+  companion object {
+    private const val API_URL = "https://example.com/?send_report"
+    private const val PIC_FROM_GALLERY = 2
+    private const val REPORT_APP_ID = 46341L
+    private const val REPORT_PROVIDER_ID = 46341L
+    private const val REPORT_SESSION_KEY = "session_key_in_next_chapter"
+  }
+
+  object ReportTracker {
+    var reportNumber = AtomicInteger()
+  }
+
+  @Volatile
+  private var isSendingReport = false
+
+  private val binding get() = _binding!!
+  private var _binding: FragmentReportDetailBinding? = null
+
+  override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+      savedInstanceState: Bundle?): View? {
+    _binding = FragmentReportDetailBinding.inflate(inflater, container, false)
+
+    binding.sendButton.setOnClickListener {
+      sendReportPressed()
+    }
+
+    binding.uploadPhotoButton.setOnClickListener {
+      uploadPhotoPressed()
+    }
+
+    return binding.root
+  }
+
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+
+    setupUI()
+  }
+
+  override fun onPause() {
+    context?.cacheDir?.deleteRecursively()
+    context?.externalCacheDir?.deleteRecursively()
+
+    super.onPause()
+  }
+
+  private fun setupUI() {
+    details_edtxtview.imeOptions = EditorInfo.IME_ACTION_DONE
+    details_edtxtview.setRawInputType(InputType.TYPE_CLASS_TEXT)
+  }
+
+  private fun sendReportPressed() {
+    if (!isSendingReport) {
+      isSendingReport = true
+
+      //1. Save report
+      var reportString = category_edtxtview.text.toString()
+      reportString += " : "
+      reportString += details_edtxtview.text.toString()
+      val reportID = UUID.randomUUID().toString()
+
+      context?.let { theContext ->
+        val file = File(theContext.filesDir?.absolutePath, "$reportID.txt")
+        file.bufferedWriter().use {
+          it.write(reportString)
+        }
+      }
+
+      ReportTracker.reportNumber.incrementAndGet()
+
+      //2. Send report
+      val postParameters = mapOf("application_id" to REPORT_APP_ID * REPORT_PROVIDER_ID,
+          "report_id" to reportID,
+          "report" to reportString)
+      if (postParameters.isNotEmpty()) {
+        //send report
+        val connection = URL(API_URL).openConnection() as HttpsURLConnection
+        //...
+      }
+
+      isSendingReport = false
+      context?.let {
+        val report = "Report: ${ReportTracker.reportNumber.get()}"
+        val toast = Toast.makeText(it, "Thank you for your report.$report", Toast
+            .LENGTH_LONG)
+        toast.show()
+      }
+
+      val inputMethodManager = activity?.getSystemService(Context.INPUT_METHOD_SERVICE) as
+      InputMethodManager
+      inputMethodManager.hideSoftInputFromWindow(view?.windowToken, 0)
+    }
+  }
+
+  private fun uploadPhotoPressed() {
+    context?.let {
+      if (ContextCompat.checkSelfPermission(it, Manifest.permission.READ_EXTERNAL_STORAGE)
+          != PackageManager.PERMISSION_GRANTED) {
+        requestPermissions(arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.READ_EXTERNAL_STORAGE), PIC_FROM_GALLERY)
+      } else {
+        val galleryIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        startActivityForResult(galleryIntent, PIC_FROM_GALLERY)
+      }
+    }
+  }
+
+  override fun onRequestPermissionsResult(requestCode: Int,
+                                          permissions: Array<String>, grantResults: IntArray) {
+    when (requestCode) {
+      PIC_FROM_GALLERY -> {
+        // If request is cancelled, the result arrays are empty.
+        if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+          // Permission was granted
+          val galleryIntent = Intent(Intent.ACTION_PICK,
+              MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+          startActivityForResult(galleryIntent, PIC_FROM_GALLERY)
+        }
+        return
+      }
+      else -> {
+        // Ignore all other requests.
+      }
+    }
+  }
+
+  override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+    super.onActivityResult(requestCode, resultCode, data)
+
+    when (requestCode) {
+
+      PIC_FROM_GALLERY ->
+
+        if (resultCode == Activity.RESULT_OK) {
+
+          //image from gallery
+          val selectedImage = data?.data
+          selectedImage?.let {
+            showUploadedFile(selectedImage)
+          }
+        }
+      else -> println("Didn't select picture option")
+    }
+  }
+
+  private fun showUploadedFile(selectedImage: Uri) {
+    //get filename
+    val fileNameColumn = arrayOf(MediaStore.Images.Media.DISPLAY_NAME)
+    val nameCursor = activity?.contentResolver?.query(selectedImage, fileNameColumn,
+        null, null, null)
+    nameCursor?.moveToFirst()
+    val nameIndex = nameCursor?.getColumnIndex(fileNameColumn[0])
+    var filename = ""
+    nameIndex?.let {
+      filename = nameCursor.getString(it)
+    }
+    nameCursor?.close()
+
+    //update UI with filename
+    upload_status_textview?.text = filename
+  }
+
+}
---
+++ 13-user-privacy/projects/starter/app/src/main/java/com/raywenderlich/android/petsave/search/presentation/SearchFragmentViewModel.kt
@@ -0,0 +1,240 @@
+/*
+ * Copyright (c) 2020 Razeware LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+ * distribute, sublicense, create a derivative work, and/or sell copies of the
+ * Software in any work that is designed, intended, or marketed for pedagogical or
+ * instructional purposes related to programming, coding, application development,
+ * or information technology.  Permission for such use, copying, modification,
+ * merger, publication, distribution, sublicensing, creation of derivative works,
+ * or sale is expressly withheld.
+ *
+ * This project and source code may use libraries or frameworks that are
+ * released under various Open-Source licenses. Use of those libraries and
+ * frameworks are governed by their own individual licenses.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package com.raywenderlich.android.petsave.search.presentation
+
+import androidx.hilt.Assisted
+import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.*
+import com.raywenderlich.android.logging.Logger
+import com.raywenderlich.android.petsave.core.domain.model.NoMoreAnimalsException
+import com.raywenderlich.android.petsave.core.domain.model.animal.Animal
+import com.raywenderlich.android.petsave.core.domain.model.pagination.Pagination
+import com.raywenderlich.android.petsave.core.presentation.Event
+import com.raywenderlich.android.petsave.core.presentation.model.mappers.UiAnimalMapper
+import com.raywenderlich.android.petsave.core.utils.DispatchersProvider
+import com.raywenderlich.android.petsave.core.utils.createExceptionHandler
+import com.raywenderlich.android.petsave.search.domain.model.SearchParameters
+import com.raywenderlich.android.petsave.search.domain.model.SearchResults
+import com.raywenderlich.android.petsave.search.domain.usecases.GetSearchFilters
+import com.raywenderlich.android.petsave.search.domain.usecases.SearchAnimals
+import com.raywenderlich.android.petsave.search.domain.usecases.SearchAnimalsRemotely
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.rxkotlin.addTo
+import io.reactivex.subjects.BehaviorSubject
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+class SearchFragmentViewModel @ViewModelInject constructor(
+    @Assisted private val savedStateHandle: SavedStateHandle,
+    private val getSearchFilters: GetSearchFilters,
+    private val searchAnimals: SearchAnimals,
+    private val searchAnimalsRemotely: SearchAnimalsRemotely,
+    private val uiAnimalMapper: UiAnimalMapper,
+    private val dispatchersProvider: DispatchersProvider,
+    private val compositeDisposable: CompositeDisposable
+): ViewModel() {
+
+  val state: LiveData<SearchViewState>
+    get() = _state
+
+
+  private val _state: MutableLiveData<SearchViewState> = MutableLiveData()
+  private val querySubject = BehaviorSubject.create<String>()
+  private val ageSubject = BehaviorSubject.createDefault<String>("")
+  private val typeSubject = BehaviorSubject.createDefault<String>("")
+
+  private var runningJobs = mutableListOf<Job>()
+  private var isLastPage = false
+  private var currentPage = 0
+
+  init {
+    _state.value = SearchViewState()
+  }
+
+  fun handleEvents(event: SearchEvent) {
+    when(event) {
+      is SearchEvent.PrepareForSearch -> prepareForSearch()
+      is SearchEvent.QueryInput -> updateQuery(event.input)
+      is SearchEvent.AgeValueSelected -> updateAgeValue(event.age)
+      is SearchEvent.TypeValueSelected -> updateTypeValue(event.type)
+    }
+  }
+
+  private fun prepareForSearch() {
+    loadMenuValues()
+    setupSearchSubscription()
+  }
+
+  private fun loadMenuValues() {
+    val exceptionHandler = createExceptionHandler(message = "Failed to get menu values!")
+
+    viewModelScope.launch(exceptionHandler) {
+      val (ages, types) = withContext(dispatchersProvider.io()) { getSearchFilters() }
+      updateStateWithMenuValues(ages, types)
+    }
+  }
+
+  private fun createExceptionHandler(message: String): CoroutineExceptionHandler {
+    return viewModelScope.createExceptionHandler(message) {
+      onFailure(it)
+    }
+  }
+
+  private fun updateStateWithMenuValues(ages: List<String>, types: List<String>) {
+    _state.value = state.value!!.copy(
+        ageMenuValues = Event(ages),
+        typeMenuValues = Event(types)
+    )
+  }
+
+  private fun setupSearchSubscription() {
+    searchAnimals(querySubject, ageSubject, typeSubject)
+        .observeOn(AndroidSchedulers.mainThread())
+        .doOnNext { runningJobs.map { it.cancel() } }
+        .subscribe(
+            { onSearchResults(it) },
+            { onFailure(it) }
+        )
+        .addTo(compositeDisposable)
+  }
+
+  private fun updateQuery(input: String) {
+    resetPagination()
+
+    querySubject.onNext(input)
+
+    if (input.isEmpty()) {
+      setNoSearchQueryStateIf()
+    } else {
+      setSearchingState()
+    }
+  }
+
+
+  private fun resetPagination() {
+    currentPage = 0
+    isLastPage = false
+  }
+
+  private fun setSearchingState() {
+    _state.value = state.value!!.copy(noResultsState = false)
+  }
+
+  private fun setNoSearchQueryStateIf() {
+    _state.value = state.value!!.copy(
+        noSearchQueryState = true,
+        searchResults = emptyList(),
+        noResultsState = false
+    )
+  }
+
+  private fun updateAgeValue(age: String) {
+    ageSubject.onNext(age)
+  }
+
+  private fun updateTypeValue(type: String) {
+    typeSubject.onNext(type)
+  }
+
+  private fun onSearchResults(searchResults: SearchResults) {
+    val (animals, searchParameters) = searchResults
+
+    if (animals.isEmpty()) {
+      onEmptyCacheResults(searchParameters)
+    } else {
+      onAnimalList(animals)
+    }
+  }
+
+  private fun onAnimalList(animals: List<Animal>) {
+    _state.value = state.value!!.copy(
+        noSearchQueryState = false,
+        searchResults = animals.map { uiAnimalMapper.mapToView(it) },
+        searchingRemotely = false,
+        noResultsState = false
+    )
+  }
+
+  private fun onEmptyCacheResults(searchParameters: SearchParameters) {
+    searchRemotely(searchParameters)
+    _state.value = state.value!!.copy(
+        searchingRemotely = true,
+        searchResults = emptyList()
+    )
+  }
+
+  private fun searchRemotely(searchParameters: SearchParameters) {
+    val exceptionHandler = createExceptionHandler(message = "Failed to search remotely.")
+
+    val job = viewModelScope.launch(exceptionHandler) {
+      val pagination = withContext(dispatchersProvider.io()) {
+        Logger.d("Searching remotely...")
+
+        searchAnimalsRemotely(++currentPage, searchParameters)
+      }
+
+      onPaginationInfoObtained(pagination)
+    }
+
+    runningJobs.add(job)
+
+    job.invokeOnCompletion {
+      it?.printStackTrace()
+      runningJobs.remove(job)
+    }
+  }
+
+  private fun onPaginationInfoObtained(pagination: Pagination) {
+    currentPage = pagination.currentPage
+    isLastPage = !pagination.canLoadMore
+  }
+
+  private fun onFailure(throwable: Throwable) {
+    _state.value = if (throwable is NoMoreAnimalsException) {
+      state.value!!.copy(searchingRemotely = false, noResultsState = true)
+    } else {
+      state.value!!.copy(failure = Event(throwable))
+    }
+
+  }
+
+  override fun onCleared() {
+    super.onCleared()
+    compositeDisposable.clear()
+  }
+}
