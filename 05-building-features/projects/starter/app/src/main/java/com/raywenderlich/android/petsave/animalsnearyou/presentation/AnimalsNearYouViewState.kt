0a6a6d3a8b53305326dbc0896ef30276646a747e
Ricardo Costeira <ricardo.costeira@mindera.com>
2020-11-25 01:04:25 +0000

    Adding starter project

---
+++ 05-building-features/projects/starter/app/src/main/java/com/raywenderlich/android/petsave/animalsnearyou/presentation/AnimalsNearYouViewState.kt
@@ -0,0 +1,4 @@
+package com.raywenderlich.android.petsave.animalsnearyou.presentation
+
+import com.raywenderlich.android.petsave.common.presentation.Event
+import com.raywenderlich.android.petsave.common.presentation.model.UIAnimal
