27a5eab25c1b2678ea8861b22c62714fd53ac24b
Q <q@Q.qq>
2020-11-27 15:12:41 -0500

    add project files

---
+++ 16-app-hardening/projects/starter/app/src/main/java/com/raywenderlich/android/petsave/core/domain/model/user/User.kt
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2020 Razeware LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+ * distribute, sublicense, create a derivative work, and/or sell copies of the
+ * Software in any work that is designed, intended, or marketed for pedagogical or
+ * instructional purposes related to programming, coding, application development,
+ * or information technology.  Permission for such use, copying, modification,
+ * merger, publication, distribution, sublicensing, creation of derivative works,
+ * or sale is expressly withheld.
+ *
+ * This project and source code may use libraries or frameworks that are
+ * released under various Open-Source licenses. Use of those libraries and
+ * frameworks are governed by their own individual licenses.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package com.raywenderlich.android.petsave.core.domain.model.user
+
+import org.simpleframework.xml.Element
+import org.simpleframework.xml.Root
+import java.io.Serializable
+
+@Root(name = "user", strict = false)
+data class User constructor(
+    @field:Element(name = "name")
+    @param:Element(name = "name")
+    var name: String,
+
+    @field:Element(name = "password")
+    @param:Element(name = "password")
+    var password: String,
+
+    @field:Element(name = "id")
+    @param:Element(name = "id")
+    var id: String,
+
+    @field:Element(name = "extra")
+    @param:Element(name = "extra")
+    var extra: String) : Serializable
