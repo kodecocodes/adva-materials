eb66ea47f1fb2b590eaa5ec8259c4a5fb41a1670
Ricardo Costeira <ricardo.costeira@mindera.com>
2020-11-28 20:43:50 +0000

    Fixed typo on ViewModel name

--- 05-building-features/projects/final/app/src/main/java/com/raywenderlich/android/petsave/animalsnearyou/domain/usecases/GetAnimals.kt
+++ 05-building-features/projects/final/app/src/main/java/com/raywenderlich/android/petsave/animalsnearyou/domain/usecases/GetAnimals.kt
@@ -37,8 +37,7 @@ package com.raywenderlich.android.petsave.animalsnearyou.domain.usecases
 import com.raywenderlich.android.petsave.common.domain.repositories.AnimalRepository
 import javax.inject.Inject
 
-class GetAnimals @Inject constructor(
-    private val animalRepository: AnimalRepository){
+class GetAnimals @Inject constructor(private val animalRepository: AnimalRepository) {
   operator fun invoke() = animalRepository.getAnimals()
       .filter { it.isNotEmpty() }
 }
