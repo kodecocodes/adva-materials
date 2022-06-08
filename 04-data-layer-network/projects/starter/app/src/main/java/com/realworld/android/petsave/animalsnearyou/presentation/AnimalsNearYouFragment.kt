730798c985299be35cfbdc37201f836e0d02e251
Ricardo Costeira <ricardo.costeira@mindera.com>
2022-06-08 01:08:26 +0100

    Updated starter project dependencies, copyrights and with changes from previous chapters

---
+++ 04-data-layer-network/projects/starter/app/src/main/java/com/realworld/android/petsave/animalsnearyou/presentation/AnimalsNearYouFragment.kt
@@ -0,0 +1,31 @@
+package com.realworld.android.petsave.animalsnearyou.presentation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.realworld.android.petsave.databinding.FragmentAnimalsNearYouBinding
+
+class AnimalsNearYouFragment : Fragment() {
+
+    private val binding get() = _binding!!
+
+    private var _binding: FragmentAnimalsNearYouBinding? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentAnimalsNearYouBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
