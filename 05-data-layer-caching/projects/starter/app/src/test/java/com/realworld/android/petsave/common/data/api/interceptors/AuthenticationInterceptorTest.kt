a6c9f2146128a1b5366fb65f57d9450c704a42a9
Ricardo Costeira <ricardo.costeira@mindera.com>
2022-06-12 01:45:58 +0100

    updated test

--- 05-data-layer-caching/projects/starter/app/src/test/java/com/realworld/android/petsave/common/data/api/interceptors/AuthenticationInterceptorTest.kt
+++ 05-data-layer-caching/projects/starter/app/src/test/java/com/realworld/android/petsave/common/data/api/interceptors/AuthenticationInterceptorTest.kt
@@ -1,6 +1,5 @@
 package com.realworld.android.petsave.common.data.api.interceptors
 
-import android.os.Build
 import com.google.common.truth.Truth.assertThat
 import com.realworld.android.petsave.common.data.api.ApiConstants
 import com.realworld.android.petsave.common.data.api.ApiParameters
@@ -18,11 +17,9 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mockito.*
 import org.robolectric.RobolectricTestRunner
-import org.robolectric.annotation.Config
-import org.threeten.bp.Instant
+import java.time.Instant
 
 @RunWith(RobolectricTestRunner::class)
-@Config(manifest=Config.NONE, sdk = [Build.VERSION_CODES.P])
 class AuthenticationInterceptorTest {
 
   private lateinit var preferences: Preferences
