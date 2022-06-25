3b352ccdfe3422751e6701b7f6963bffa659953a
Ricardo Costeira <ricardo.costeira@mindera.com>
2022-06-25 12:59:59 +0100

    Updated dependencies, gradle config and manifest on starter project

--- 06-building-features-animals-near-you/projects/starter/app/src/main/java/com/raywenderlich/android/petsave/common/data/di/CacheModule.kt
+++ 06-building-features-animals-near-you/projects/starter/app/src/main/java/com/raywenderlich/android/petsave/common/data/di/CacheModule.kt
@@ -45,7 +45,6 @@ import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ApplicationComponent
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
