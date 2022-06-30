a8b441d7df38c7720e5526dc33551f625056817f
Ricardo Costeira <ricardo.costeira@mindera.com>
2022-06-30 02:33:32 +0100

    Simplified Pagination class in starter project.

--- 06-building-features-animals-near-you/projects/starter/app/src/main/java/com/realworld/android/petsave/common/domain/model/pagination/Pagination.kt
+++ 06-building-features-animals-near-you/projects/starter/app/src/main/java/com/realworld/android/petsave/common/domain/model/pagination/Pagination.kt
@@ -39,14 +39,7 @@ data class Pagination(
     val totalPages: Int
 ) {
 
-  companion object {
-    // For the cases when we store the current page locally, but haven't yet requested a new page
-    // from the remote source. Total pages should change with time, so we'll handle the value as
-    // unknown before updating.
-    const val UNKNOWN_TOTAL = -1
-    const val DEFAULT_PAGE_SIZE = 20
-  }
-
-  val canLoadMore: Boolean
-    get() = totalPages == UNKNOWN_TOTAL || currentPage < totalPages
+    companion object {
+        const val DEFAULT_PAGE_SIZE = 20
+    }
 }
