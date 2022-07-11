e79913f419925a565284b41eeb1f956772dd4d9d
Subhrajyoti Sen <subhrajyoti12@gmail.com>
2022-07-11 13:25:14 +0530

    Rename package

--- 21-advanced-debugging/projects/starter/logging/src/debug/java/com/raywenderlich/android/logging/TimberLogging.kt
+++
@@ -1,9 +0,0 @@
-package com.raywenderlich.android.logging
-
-import timber.log.Timber
-
-class TimberLogging: Timber.DebugTree() {
-    override fun createStackElementTag(element: StackTraceElement): String {
-        return "(${element.fileName}:${element.lineNumber}) on ${element.methodName}"
-    }
-}
