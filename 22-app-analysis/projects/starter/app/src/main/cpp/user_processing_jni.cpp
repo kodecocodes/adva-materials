26b8e4fbd160fbfba21ab0e130f062e178a0bc43
narko <6020peaks@gmail.com>
2022-07-14 19:21:34 +0200

    Carrying changes from CH18

--- 22-app-analysis/projects/starter/app/src/main/cpp/user_processing_jni.cpp
+++ 22-app-analysis/projects/starter/app/src/main/cpp/user_processing_jni.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020 Razeware LLC
+ * Copyright (c) 2022 Razeware LLC
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -47,7 +47,7 @@ public:
 
 void UserInfoHandler::UserInfoHandler()
 {
-  memset(&_passwordChar, 0, sizeof(char));
+  _passwordChar = "";
   _usernameString = "Anonymous";
 }
 
